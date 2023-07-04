caeffbfe6e422ff23ea9708a89c2dba27b5df840
sofia perone <sofiaperone62@gmail.com>
2023-07-03 23:35:22 -0300

    notifications

--- src/components/Card/WorkCard.jsx
+++ src/components/Card/WorkCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "../Portfolio/Portfolio.css"
 
-const WorkCard = ({name, description,img, category}) => {
+const WorkCard = ({name, description,img, category, link, skills}) => {
   return (
     <div className='workCard'>
       <img src={img} alt={name} />
       <h5>{category}</h5>
       <h1>{name}</h1>
       <p>{description}</p>
+      <p>Deploy : <a href={link}> {link}</a></p>
+      <p>Skills: {skills}</p>
+
+
     </div>
   )
 }
